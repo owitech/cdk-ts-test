9806d8d28f95403717f51c071800aee59a65b14b
Julio Castillo <121035941+owitech@users.noreply.github.com>
2023-03-01 23:06:05 -0500

    Initial commit

---
+++ test/ts-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TsTest from '../lib/ts-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ts-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TsTest.TsTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
